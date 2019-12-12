112b730cc6b8eef564f6ab335584ccea8356c2e6
Mateusz Szymczyk <zwierzak@MacBook-Pro-MacOS.local>
2019-12-12 14:22:41 +0100

    initialize with simple functions

---
+++ main.go
@@ -0,0 +1,30 @@
+// +build linux
+
+package main
+
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Foo() string {
+	return "foo"
+}
+func Bar() string {
+	return "bar"
+}
+func Quz(v string) string {
+	if v == "foo" {
+		return Foo()
+	}
+	if v == "bar" {
+		return Bar()
+	}
+	return "INVALID"
+}
+
+//var b = foo()
+func main() {
+	fmt.Println(strings.ToUpper("Hello GO"))
+}
---
+++ main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestFoo(t *testing.T) {
+	//t.Error("intentional error 1")
+}
+func TestBar(t *testing.T) {
+	result := Bar()
+	if result != "bar" {
+		t.Errorf("expecting bar, got %s", result)
+	}
+}
+func TestQuz(t *testing.T) {
+	result := Quz("bar")
+	if result != "bar" {
+		t.Errorf("expecting bar, got %s", result)
+	}
+	result := Quz("qux")
+	if result != "INVALID" {
+		t.Errorf("expecting INVALID, got %s", result)
+	}
+}
