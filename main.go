7bf0cfef4995531748facf7cfca1695ce208f79c
Mateusz Szymczyk <zwierzak@MacBook-Pro-MacOS.local>
2019-12-13 22:44:22 +0100

    created service for sqrt and log

--- main.go
+++ main.go
@@ -2,89 +2,67 @@ package main
 
 import (
 	"fmt"
-	"html/template"
+	"log"
+	"strconv"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/ds4tech/pipeline-calculator-ws/calculator_ws"
 )
+var dat map[string]interface{}
 
-type Page struct {
-	Title string
-	Body  []byte
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to Calculator!")
 }
+func logHandler(w http.ResponseWriter, r *http.Request) {
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the number")
+	}
+  json.Unmarshal(reqBody, &dat)
+  num := dat["number"].(string)
 
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
-}
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	fmt.Println("Loading file " + filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, err := template.ParseFiles(tmpl + ".html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	err = t.Execute(w, p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+  i, err := strconv.ParseFloat(num, 64)
+  result := calc.Log(i)
+	if err == nil {
+    jsonMap := map[string]float64{"result": result}
+    jsonResult, _ := json.Marshal(jsonMap)
+    fmt.Printf("\nLn of %v is: %v", num, result)
+		fmt.Fprintf(w, string(jsonResult)) 
 	}
 }
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	p, err := loadPage(title)
-	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-		return
-	}
-	renderTemplate(w, "view", p)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
-	}
-	renderTemplate(w, "edit", p)
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
-	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
-	p.save()
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
-
 func sqrtHandler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Dupa!")
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the number")
+	}
+  json.Unmarshal(reqBody, &dat)
+  num := dat["number"].(string)
+
+  i, err := strconv.ParseFloat(num, 64)
+  result := calc.Sqrt(i)
+	if err == nil {
+    jsonMap := map[string]float64{"result": result}
+    jsonResult, _ := json.Marshal(jsonMap)
+    fmt.Printf("\nSqrt of %v is: %v", num, result)
+		fmt.Fprintf(w, string(jsonResult)) 
+	}
 }
+
 
 func main() {
 	fmt.Printf("Sum of %d and %d is %d\n", 3,4, calc.Sum(3,4))
 	fmt.Printf("Factorial of 4 is %v\n", calc.Factorial(4))
   fmt.Printf("4 is prime: %v\n", calc.IsPrime(4))
   fmt.Printf("3 is prime: %v\n", calc.IsPrime(3))
-  fmt.Printf("Log(9) = %v\n", calc.Log(9))
 	fmt.Println("End of calculations")
 
 	fmt.Println("Starting server...")
- 
-	http.HandleFunc("/sqrt/", sqrtHandler)
-
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
-	http.ListenAndServe(":8080", nil)
-	//	p1 := &Page{Title: "TestPage", Body: []byte("Simple Page.")}
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/sqrt", sqrtHandler).Methods("POST")
+	router.HandleFunc("/log", logHandler).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
