fa5f29fe460461358639cf6ad572a965497d9732
Mateusz Szymczyk <zwierzak@MacBook-Pro-MacOS.local>
2019-12-13 15:08:29 +0100

    new web service

--- main.go
+++ main.go
@@ -66,16 +66,21 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+func sqrtHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Dupa!")
+}
+
 func main() {
-	fmt.Println("Starting server...")
- 
-	//"strings"
 	fmt.Printf("Sum of %d and %d is %d\n", 3,4, calc.Sum(3,4))
 	fmt.Printf("Factorial of 4 is %v\n", calc.Factorial(4))
   fmt.Printf("4 is prime: %v\n", calc.IsPrime(4))
   fmt.Printf("3 is prime: %v\n", calc.IsPrime(3))
   fmt.Printf("Log(9) = %v\n", calc.Log(9))
 	fmt.Println("End of calculations")
+
+	fmt.Println("Starting server...")
+ 
+	http.HandleFunc("/sqrt/", sqrtHandler)
 
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
