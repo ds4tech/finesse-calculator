5e1d8d9a734226616217b7cc21b6bd9cdd97dc41
Mateusz Szymczyk <zwierzak@MacBook-Pro-MacOS.local>
2019-12-13 14:29:40 +0100

    implemented 5 simple math functions

--- calculator_ws/main.go
+++ calculator_ws/main.go
@@ -4,34 +4,48 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/big"
 	"strings"
 )
 
-func sqrt(x float64) string {
-  if x < 0 {
-    return sqrt(-x) + "i"
+func Sum(x int, y int) int {
+  return x+y
+}
+
+func Sqrt(x float64) float64 {
+  return math.Sqrt(x)
+}
+
+func Factorial(n uint64) (result uint64) {
+  if (n>0) {
+    result = n * Factorial(n-1)
+    return result
   }
-  return fmt.Sprint(math.Sqrt(x))
+  return 1
 }
 
-func Foo() string {
-	return "foo"
-}
-func Bar() string {
-	return "bar"
-}
-func Quz(v string) string {
-	if v == "foo" {
-		return Foo()
-	}
-	if v == "bar" {
-		return Bar()
-	}
-	return "INVALID"
+func isPrime(n int) (result bool) {
+  for i := 2; i <= n; i++ {
+    if big.NewInt(int64(i)).ProbablyPrime(20) {
+      result = true
+    } else {
+      result = false 
+    }
+  }
+  return result
 }
 
-//var b = foo()
+func Log(n float64) (result float64) {
+  result = math.Log(n)
+  return result
+}
+
 func main() {
 	fmt.Println(strings.ToUpper("Hello GO"))
-  fmt.Println(sqrt(2), sqrt(-4))
+	fmt.Printf("Sum of %d and %d is %d\n", 3,4, Sum(3,4))
+	fmt.Printf("Factorial of 4 is %v\n", Factorial(4))
+  fmt.Printf("4 is prime: %v\n", isPrime(4))
+  fmt.Printf("3 is prime: %v\n", isPrime(3))
+  fmt.Printf("Log(9) = %v\n", Log(9))
+	fmt.Println("End of calculations")
 }
--- calculator_ws/main_test.go
+++ calculator_ws/main_test.go
@@ -4,22 +4,36 @@ import (
 	"testing"
 )
 
-func TestFoo(t *testing.T) {
-	//t.Error("intentional error 1")
+func TestSum(t *testing.T) {
+  result := Sum(int(3), int(4))
+  expecting := 8
+  if result != expecting {
+		t.Errorf("expecting %d, got %d", expecting, result)
+  }
 }
-func TestBar(t *testing.T) {
-	result := Bar()
-	if result != "bar" {
-		t.Errorf("expecting bar, got %s", result)
+func TestSqrt(t *testing.T) {
+  result := Sqrt(9)
+  expecting := float64(3)
+  if result != expecting {
+		t.Errorf("expecting %v, got %v", expecting, result)
+  }
+  result = Sqrt(-4)
+  expecting = 2
+  if result != expecting {
+		t.Errorf("expecting %v, got %v", expecting, result)
+  }
+}
+func TestFactorial(t *testing.T) {
+	result := Factorial(4)
+  expecting := uint64(24)
+  if result != expecting {
+		t.Errorf("expecting %d, got %d", expecting, result)
 	}
 }
-func TestQuz(t *testing.T) {
-	result := Quz("bar")
-	if result != "bar" {
-		t.Errorf("expecting bar, got %s", result)
-	}
-	result = Quz("qux")
-	if result != "INVALID" {
-		t.Errorf("expecting INVALID, got %s", result)
-	}
+func TestLog(t *testing.T) {
+  result := Log(1)
+  expecting := float64(0)
+  if result != expecting {
+		t.Errorf("expecting %v, got %v", expecting, result)
+  }
 }
