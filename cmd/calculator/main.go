f313d9b289d43b1949b25007c2533f1e8fc88233
Mateusz Szymczyk <zwierzak@MacBook-Pro-MacOS.local>
2019-12-22 16:50:44 +0100

    moved handlers to routes added Route struct

--- cmd/calculator/main.go
+++ cmd/calculator/main.go
@@ -4,20 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 
 func main() {
 	fmt.Println("Starting server...\n")
 
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", homeLink)
-	router.HandleFunc("/sum", sumHandler).Methods("POST")
-	router.HandleFunc("/sqrt", sqrtHandler).Methods("POST")
-	router.HandleFunc("/log", logHandler).Methods("POST")
-	router.HandleFunc("/factorial", factorialHandler).Methods("POST")
-	router.HandleFunc("/isPrime", isPrimeHandler).Methods("POST")
+	router := NewRouter()
 	log.Fatal(http.ListenAndServe(":8888", router))
 }
---
+++ cmd/calculator/routes.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+
+	for _, route := range routes {
+
+		var handler http.Handler
+		handler = route.HandlerFunc
+//		handler = Logger(handler, route.Name)
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(handler)
+		//Handler(route.HandlerFunc)
+	}
+	return router
+}
+
+var routes = Routes{
+	Route{
+		"Index",
+		"GET",
+		"/",
+		homeLink,
+	},
+	Route{
+		"sum",
+		"POST",
+		"/sum",
+		sumHandler,
+	},
+	Route{
+		"sqrt",
+		"POST",
+		"/sqrt",
+		sqrtHandler,
+	},
+	Route{
+		"factorial",
+		"POST",
+		"/factorial",
+		factorialHandler,
+	},
+	Route{
+		"logHandler",
+		"POST",
+		"/log",
+		logHandler,
+	},
+	Route{
+		"isPrime",
+		"POST",
+		"/isPrime",
+		isPrimeHandler,
+	},
+}
